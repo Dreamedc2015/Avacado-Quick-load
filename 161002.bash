613f77654f5c05b7e7f8669e582580e39ae1b9f7
Dreamedc2015 <66358133+Dreamedc2015@users.noreply.github.com>
2021-02-22 11:17:20 +0000

    Update 161002.bash

--- 161002.bash
+++ 161002.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 open /Applications/Minecraft\ Bedrock\ Launcher.app/Contents/MacOS/msa-daemon 
 cd /Applications/Minecraft\ Bedrock\ Launcher.app/Contents/MacOS
-./mcpelauncher-client -dg ~/Library/Application\ Support/mcpelauncher/versions/1.16.40.02
+./mcpelauncher-client -dg ~/Library/Application\ Support/mcpelauncher/versions/$1
